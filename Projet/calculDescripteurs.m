98bb9651535e580eea61977753b4ce82fdd8123b
frtoud <francois.toulouse@polymtl.ca>
2018-12-03 00:02:55 -0500

    calculDescripteurs: bins in progress

--- Projet/calculDescripteurs.m
+++ Projet/calculDescripteurs.m
@@ -1,8 +1,16 @@
-function [descs] = calculDescripteurs(points, imgsize, gradients, resolution_octave)
+function [descs] = calculDescripteurs(points, imgsize, images, resolution_octave)
+  
+  [gradients, magnitudes] = calculGradients(images);
+  %Assume results to be:
+  %dim 1: octave
+  %dim 2: position in octave
+  %dim 3: x
+  %dim 4: y
   
   lambda_descr = 6;
   half_sample_size = 8; % sera multiplié par deux pour garantir un chiffre pair
   sample_size = half_sample_size * 2;
+  feat_size = 4;
   
   [n, m] = size(points);
   descs = zeros(n, 130) % X + Y + 128 descriptors
@@ -20,24 +28,58 @@ function [descs] = calculDescripteurs(points, imgsize, gradients, resolution_oct
       sigma = points(index_p, 5);
       angle = points(index_p, 6);
       
-      desc_offset = 3; %Commencer a inserer des descripteurs ici
-      
-      samples = zeros(sample_size);
+      samples_grad = zeros(sample_size);
+      samples_mag = zeros(sample_size);
       res = resolution_octave(oct);
       step = lambda_descr*sigma*2/sample_size;
+      
+      target_gradient = gradients(oct, num)
+      target_magnitude = magnitudes(oct, num)
+      
+      exp_det = 2 * (lambda_descr * sigma)^2;
+      
+      %bins: 4x4x8
+      features = zeros([4, 4, 8]);
+      
+      %sample size 16
       for i = 1:sample_size
         for j = 1:sample_size
+          %la distance entre chaque point de sampling avec le step calculé
           dx = (i - half_sample_size - 0.5)*step;
           dy = (j - half_sample_size - 0.5)*step;
-          samplingx = ()/sigma
-          samplingy = (dy - half_sample_size - 0.5)*sin(angle) + posy
-          samples(dx, dy) = target_gradient()
+          %original imagespace
+          samplingx = dx*cos(angle) + dy*sin(angle) + posx;
+          samplingy = dy*cos(angle) - dx*sin(angle) + posy;
+          %corresponding octave's space
+          oct_x = floor(samplingx/res);
+          oct_y = floor(samplingy/res);
+          %scale according to gauss
+          gaussfactor = exp(-(dx^2 + dy^2)/exp_det);
+          %collect sample orientation & magnitude
+          samples_grad(dx, dy) = target_gradient(oct_x, oct_y);
+          samples_mag(dx, dy) = target_magnitude(oct_x, oct_y) * gaussfactor;
+          
+          %place in histogram features
+          
         endfor
+      endfor
+      
+      %compute feature vector
+      bins_1D = features(:);
+      SRSS = 0;
+      for f = 1:size(bins_1D)
+        SRSS = SRSS + bins_1D(f)^2;
+      endfor
+      SRSS = sqrt(SRSS); %128-dim Euclidian norm
+      for f = 1:size(bins_1D)
+        val = min(bins_1D(f), 0.2*SRSS);
+        val = min(floor(512*val/SRSS), 255)
+        %Offset de 2 pour X, Y
+        descs(index_d, f + 2) = int(val);
       endfor
       
       index_d  = index_d + 1;
     endif 
-    
     
   endfor
 endfunction
